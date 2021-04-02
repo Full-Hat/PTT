981ce57686f31960e8c63a7a7a3e920921ec35b0
Full-Hat <68519677+Full-Hat@users.noreply.github.com>
2021-04-02 15:54:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace lab4_task2
 {
     unsafe class Program
     {
-        [DllImport("lab4-task2DLL.dll", EntryPoint = "getL", CharSet = CharSet.Ansi)]
-        public static extern bool getL(StringBuilder message);
+        [DllImport("lab4-task2DLL.dll", EntryPoint = "checkPass", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool checkPass(StringBuilder message);
+        [DllImport("lab4-task2DLL.dll", EntryPoint = "botDefense", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool botDefense(StringBuilder message);
 
         static void Main(string[] args)
         {
@@ -17,14 +19,27 @@ namespace lab4_task2
             string password = Console.ReadLine();
             str.Append(password);
 
-            while(!getL(str))
+            while(!checkPass(str))
             {
                 Console.WriteLine("Wrong password, try again..");
                 str.Clear();
                 str.Append(Console.ReadLine());
             }
 
-            Console.WriteLine("You are finally enter");
+            Console.Write("Bot defense pass: ");
+            password = Console.ReadLine();
+            str.Clear();
+            str.Append(password);
+
+
+            while (!botDefense(str))
+            {
+                Console.WriteLine("Wrong password, try again..");
+                str.Clear();
+                str.Append(Console.ReadLine());
+            }
+
+            Console.WriteLine("Finally, correct password! :D");
         }
     }
 }
--- source.cpp
+++ source.cpp
@@ -3,7 +3,12 @@
 #include <iostream>
 #include <string>
 
-extern "C" __declspec(dllexport) const bool __stdcall getL(const char message[])
+extern "C" __declspec(dllexport) bool __cdecl checkPass(const char message[])
 {
 	return std::strcmp(message, "1234") == 0;
+}
+
+extern "C" __declspec(dllexport) bool __stdcall botDefense(const char message[])
+{
+	return std::strcmp(message, "VERY STRONG DEFENSE") || std::strcmp(message, "But please)");
 }
