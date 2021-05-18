e0dfef9b8684db7be2ad487eabbf4176005a50bf
Full-Hat <68519677+Full-Hat@users.noreply.github.com>
2021-05-18 20:37:02 +0300

    Add files via upload

---
+++ Types.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Analyzer
+{
+    public class TypeAn
+    {
+        public TypeAn(string _name = "")
+        {
+            Name = _name;
+        }
+
+        private string name;
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    };
+
+    class TypesAn
+    {
+        public static List<TypeAn> Types { get; set; } = new List<TypeAn>();
+
+        static TypesAn()
+        {
+            Types.Add(new TypeAn("int"));
+            Types.Add(new TypeAn("double"));
+            Types.Add(new TypeAn("void"));
+        }
+        public static bool IsType(string str)
+        {
+            foreach (TypeAn type in TypesAn.Types)
+            {
+                if (str == type.Name)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public TypeAn this[int index] => Types[index];
+    };
+}
---
+++ Utils.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace Analyzer
+{
+    class Utils
+    {
+        public static List<string> Split(string data)
+        {
+            bool isPrWord = false;
+
+            List<string> words = new List<string>();
+
+            foreach (char symbol in data)
+            {
+                if (symbol != ' ' && !isPrWord)
+                {
+                    words.Add(string.Empty);
+                }
+
+                if (symbol != ' ')
+                {
+                    words[words.Count - 1] += symbol;
+                }
+
+                isPrWord = symbol != ' ';
+            }
+
+            return words;
+        }
+
+        public static string GetWord(string data, int index)
+        {
+            bool isPrWord = false;
+
+            string word = string.Empty;
+            int wordInd = 0;
+
+            foreach (char symbol in data)
+            {
+                if (symbol != ' ' && !isPrWord && word != "" && wordInd == index++)
+                {
+                    return word;
+                }
+
+                if (symbol != ' ')
+                {
+                    word += symbol;
+                }
+
+                isPrWord = symbol != ' ';
+            }
+
+            return "";
+        }
+
+        public static string TrimFirst(string str)
+        {
+            string newString = string.Empty;
+
+            bool isStarted = false;
+            foreach (char el in str)
+            {
+                if (el != ' ' && el != '\n' && el != '\t')
+                {
+                    isStarted = true;
+                }
+
+                if (isStarted == true)
+                {
+                    newString += el;
+                }
+            }
+
+            return newString;
+        }
+    }
+}
